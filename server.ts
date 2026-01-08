96d756cb058806ae7c5b925d7cebd7d70fea35d8
Wallace Frota <frotadev@gmail.com>
2026-01-08 17:13:39 -0300

    :recycle: refactor: server init

--- server.ts
+++ server.ts
@@ -1,6 +1,32 @@
 import app from "./src/app/";
 import { config } from "./src/app/config";
+import dbConnection from "./src/app/config/database.config";
+import { Comment, Post, User } from "./src/app/models/associations";
 
-app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
+async function startConnection() {
+  try {
+    await dbConnection.authenticate();
+    await User.sync();
+    await Post.sync();
+    await Comment.sync();
+    await dbConnection.sync();
+  } catch (error) {
+    throw error
+  }
+}
+
+async function startServer() {
+  try {
+    await startConnection();
+
+    app.listen(config.port, () => {
+      console.log(`🚀 Server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+startServer().catch((error) => {
+  console.error(error)
 });
