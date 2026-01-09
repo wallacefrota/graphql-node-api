d4a6a11cd9c91289fc6833ab721a3d54702c4478
Wallace Frota <frotadev@gmail.com>
2026-01-09 08:23:30 -0300

    :sparkles: feat(server): imported graphql server

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
+import graphqlServer from "./infra/graphql/schema";
 const app = express();
-import graphqlServer from "./infra/graphql/server";
 
 app.use("/api/graphql", graphqlServer);
 
